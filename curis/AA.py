35bcca5e19635dab9bd33bea3a150fbae6251a8e
Varun Shenoy <vnshenoy@stanford.edu>
2020-04-30 19:00:56 -0700

    added AA

--- curis/AA.py
+++ curis/AA.py
@@ -41,12 +41,51 @@ class AInterval:
                 new_noise[e] = noise_val
             return AInterval(new_base, new_noise)
         else:
-            raise NotImplementedError("TODO: adding a constant")
+            return AInterval(self.base + rhs, self.noise)
 
     def __sub__(self, rhs):
-        raise NotImplementedError("TODO: subtract AInterval or a constant")
+        if isinstance(rhs, AInterval):
+            new_base = self.base - rhs.base
+            new_noise = self.noise
+            es = set(rhs.noise.keys())
+            for e in es:
+                noise_val = 0
+                for noise in (self.noise, rhs.noise):
+                    if e in noise:
+                        noise_val -= noise[e]
+                new_noise[e] = noise_val
+            return AInterval(new_base, new_noise)
+        else:
+            return AInterval(self.base - rhs, self.noise)
 
     def __mul__(self, rhs):
-        raise NotImplementedError("TODO: multiply")
+        if isinstance(rhs, AInterval):
+            x_sum = 0
+            y_sum = 0
 
+            es1 = self.noise.copy()
+            for key in es1:
+                x_sum += abs(es1[key])
+                es1[key] *=  rhs.base
 
+            es2 = rhs.noise.copy()
+            for key in es2:
+                y_sum += abs(es2[key])
+                es2[key] *=  self.base
+
+            new_noise = self.merge_dicts(es1, es2)
+            new_noise[max(set(new_noise.keys())) + 1] = x_sum * y_sum
+            return AInterval(self.base * rhs.base, new_noise)
+        else:
+            for key in self.noise:
+                self.noise[key] *=  rhs
+            return AInterval(self.base * rhs, self.noise)
+
+    def merge_dicts(self, dict1, dict2):
+        new_dict = dict1
+        for key in set(dict2.keys()):
+            if key in new_dict:
+                new_dict[key] += dict2[key]
+            else:
+                new_dict[key] = dict2[key]
+        return new_dict
