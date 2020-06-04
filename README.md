e0ad29f06d082f3e046b934c80f76f24444b953f
Ross <rdaly525@stanford.edu>
2020-06-04 08:39:38 -0700

    Added readme travis token +setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-
+[![Build Status](https://travis-ci.org/rdaly525/Curis.svg?branch=master)](https://travis-ci.org/rdaly525/Curis)
 This is a readme
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+import sys
+
+setup(
+    name='Curis',
+    version='0.0.1',
+    url='https://github.com/rdaly525/Curis',
+    license='MIT',
+    maintainer='Ross Daly',
+    maintainer_email='rdaly525@stanford.edu',
+    description='Learning Precision',
+    packages=[
+        "Curis",
+    ],
+    install_requires=[
+    ],
+    python_requires='>=3.7'
+)
