0abe223653902546c44009513ecf8c068a9aa869
Varun Shenoy <vnshenoy@stanford.edu>
2020-06-23 16:36:07 -0700

    added full testing suite for fig. 3

--- BitFlow/Precision.py
+++ BitFlow/Precision.py
@@ -12,7 +12,10 @@ class FPEpsilon:
     def __str__(self):
         return f" +{self.val}*2^(-FB{self.node}-1)ε_{self.node}" if self.val >= 0 else f" {self.val} * 2^(FB{self.node}-1)ε_{self.node}"
 
-class ErrorMultiplier:
+    def __eq__(self, rhs):
+        return self.val == rhs.val
+
+class FPEpsilonMultiplier:
     def __init__(self, Ex, Ey, val=1):
         assert isinstance(Ex, list)
         assert isinstance(Ey, list)
@@ -34,6 +37,9 @@ class ErrorMultiplier:
     def __str__(self):
         self.output = f" +{self.val}*"
         return self.generate_print()
+
+    def __eq__(self, rhs):
+        return self.Ex == rhs.Ex and self.Ey == rhs.Ey and self.val == rhs.val
 
 
 class PrecisionNode:
@@ -79,7 +85,7 @@ class PrecisionNode:
         assert isinstance(rhs, PrecisionNode)
         assert isinstance(symbol, str)
 
-        mixed_err = ErrorMultiplier(copy.deepcopy(rhs.error), copy.deepcopy(self.error))
+        mixed_err = FPEpsilonMultiplier(copy.deepcopy(rhs.error), copy.deepcopy(self.error))
 
         rhs_error = copy.deepcopy(rhs.error)
         for (i, error) in enumerate(rhs_error):
@@ -94,3 +100,6 @@ class PrecisionNode:
 
         # Store multiplication of errors as a tuple whose first element is E_x and second element is E_y
         return PrecisionNode(self.val * rhs.val, symbol, total_err)
+
+    def __eq__(self, rhs):
+        return self.error == rhs.error and self.val == rhs.val
