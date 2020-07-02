41fc923556feec4c9140efaf2183bb2593b5b7c0
Ross <rdaly525@stanford.edu>
2020-07-02 07:22:34 -0700

    Fixes imports

--- tests/test_torch/test_round.py
+++ tests/test_torch/test_round.py
@@ -1,6 +1,6 @@
-from BitFlow.torch.functions import Round
+from BitFlow.torch.functions import IntRound
 import torch as t
 
 def test_round():
     x = t.Tensor([1.2, 3.7])
-    assert t.all(Round(x) == t.Tensor([1.0, 4.0]))
+    assert t.all(IntRound(x) == t.Tensor([1.0, 4.0]))
