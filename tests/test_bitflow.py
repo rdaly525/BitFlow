66c4cc3747095b9733be10ac373712fee0b9945b
Varun Shenoy <vnshenoy@stanford.edu>
2020-07-30 12:36:26 -0700

    updated requirements.txt

--- tests/test_bitflow.py
+++ tests/test_bitflow.py
@@ -10,59 +10,59 @@ from BitFlow.utils import Imgs2Dataset
 from torch.utils import data
 
 
-# def test_image2data():
-#     converter = Imgs2Dataset()
-#     dataset, size = converter.convert_rgb2ycbcr('./data/imgs/')
-#     train_set, test_set = torch.utils.data.random_split(
-#         dataset, [int(round(0.8 * size)), int(round(0.2 * size))])
+def test_image2data():
+    converter = Imgs2Dataset()
+    dataset, size = converter.convert_rgb2ycbcr('./data/imgs/')
+    train_set, test_set = torch.utils.data.random_split(
+        dataset, [int(round(0.8 * size)), int(round(0.2 * size))])
 
-#     return
-
-
-# def gen_fig3():
-#     # (a*b) + 4 - b
-#     a = Input(name="a")
-#     b = Input(name="b")
-#     c = Constant(4.3, name="c")
-#     d = Mul(a, b, name="d")
-#     e = Add(d, c, name="e")
-#     z = Sub(e, b, name="z")
-
-#     fig3_dag = Dag(outputs=[z], inputs=[a, b])
-#     return fig3_dag
+    return
 
 
-# def gen_ex1():
-#     # (a * b) + (b * c)
-#     a = Input(name="a")
-#     b = Input(name="b")
-#     c = Input(name="c")
-#     d = Mul(a, b, name="d")
-#     e = Mul(b, c, name="e")
-#     z_1 = Add(e, d, name="z_1")
-#     z_2 = Add(a, d, name="z_2")
+def gen_fig3():
+    # (a*b) + 4 - b
+    a = Input(name="a")
+    b = Input(name="b")
+    c = Constant(4.3, name="c")
+    d = Mul(a, b, name="d")
+    e = Add(d, c, name="e")
+    z = Sub(e, b, name="z")
 
-#     dag = Dag(outputs=[z_1, z_2], inputs=[a, b, c])
-#     return dag
-
-
-# def test_fig3():
-
-#     dag = gen_fig3()
-
-#     bf = BitFlow(dag, {"z": 8.}, {'a': (-3., 2.),
-#                                   'b': (4., 8.)}, lr=8e-4, range_lr=5e-4, train_range=True, training_size=10000, testing_size=2000, distribution=0)
-#     bf.train(epochs=20)
-#     return
+    fig3_dag = Dag(outputs=[z], inputs=[a, b])
+    return fig3_dag
 
 
-# def test_ex1():
-#     dag = gen_ex1()
+def gen_ex1():
+    # (a * b) + (b * c)
+    a = Input(name="a")
+    b = Input(name="b")
+    c = Input(name="c")
+    d = Mul(a, b, name="d")
+    e = Mul(b, c, name="e")
+    z_1 = Add(e, d, name="z_1")
+    z_2 = Add(a, d, name="z_2")
 
-#     bf = BitFlow(dag, {"z_1": 5., "z_2": 8.}, {
-#         'a': (-3., 2.), 'b': (4., 8.), 'c': (-1., 1.)}, train_range=True)
-#     bf.train(epochs=10)
-#     return
+    dag = Dag(outputs=[z_1, z_2], inputs=[a, b, c])
+    return dag
+
+
+def test_fig3():
+
+    dag = gen_fig3()
+
+    bf = BitFlow(dag, {"z": 8.}, {'a': (-3., 2.),
+                                  'b': (4., 8.)}, lr=8e-4, range_lr=5e-4, train_range=True, training_size=10000, testing_size=2000, distribution=0)
+    bf.train(epochs=20)
+    return
+
+
+def test_ex1():
+    dag = gen_ex1()
+
+    bf = BitFlow(dag, {"z_1": 5., "z_2": 8.}, {
+        'a': (-3., 2.), 'b': (4., 8.), 'c': (-1., 1.)}, train_range=True)
+    bf.train(epochs=10)
+    return
 
 
 def RGB_to_YCbCr():
@@ -82,28 +82,28 @@ def RGB_to_YCbCr():
     return casestudy_dag
 
 
-# def test_rgb_case_study():
-#     dag = RGB_to_YCbCr()
+def test_rgb_case_study():
+    dag = RGB_to_YCbCr()
 
-#     params = dict(
-#         training_size=200,
-#         testing_size=20,
-#         batch_size=16,
-#         lr=1e-4,
-#         train_range=True,
-#         range_lr=1e-4,
-#         distribution=0
-#     )
+    params = dict(
+        training_size=200,
+        testing_size=20,
+        batch_size=16,
+        lr=1e-4,
+        train_range=True,
+        range_lr=1e-4,
+        distribution=0
+    )
 
-#     bf = BitFlow(dag, {"col_1": 10., "col_2": 10., "col_3": 10.}, {
-#         'r': (0., 255.), 'b': (0., 255.), 'g': (0., 255.)}, **params)
-#     bf.train(epochs=10)
+    bf = BitFlow(dag, {"col_1": 10., "col_2": 10., "col_3": 10.}, {
+        'r': (0., 255.), 'b': (0., 255.), 'g': (0., 255.)}, **params)
+    bf.train(epochs=10)
 
-#     # Sample Matrix Product
-#     test = {"r": 2., "g": 4., "b": -3., "P": bf.P, "R": bf.R, "O": bf.O}
-#     print(bf.model(**test))
+    # Sample Matrix Product
+    test = {"r": 2., "g": 4., "b": -3., "P": bf.P, "R": bf.R, "O": bf.O}
+    print(bf.model(**test))
 
-#     return
+    return
 
 
 def test_rgb_case_study_custom_dataset():
@@ -135,7 +135,7 @@ def test_rgb_case_study_custom_dataset():
 
     bf = BitFlow(dag, {"col_1": 0., "col_2": 0., "col_3": 0.}, {
         'r': (0., 255.), 'b': (0., 255.), 'g': (0., 255.)}, **params)
-    bf.train(epochs=10)
+    bf.train(epochs=1)
 
     # Sample Matrix Product
     test = {"r": 125., "g": 125., "b": 125., "P": bf.P, "R": bf.R, "O": bf.O}
